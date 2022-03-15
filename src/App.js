bc7af072f00703fe477325d8a78549b04bb12c79
jjenny7 <jmlee9707@naver.com>
2022-03-16 00:56:32 +0900

    7-2

--- src/App.js
+++ src/App.js
@@ -1,62 +1,41 @@
+// 7-2
 
-  // 7-1 투두리스트 만들기 
+// 코인 API에는 이미 key가 있으므로 안 가져와도 됨
 
-  // map() 함수
-  // -> 배열을 가지고 있을 때 각각의 element들을 바꿀 수 있게 해줌map() 은 ()에 함수를 넣을 수 있는데 배열의 모든 item에 대해 실행됨
-  // 즉 배열에 6개의 item이 있다면 6번 함수가 실행됨
-  // 그리고 그 함수로부터 내가 return한 값은 새로운 배열에 들어가게 함
-  // [‘a’, ‘b’, ‘c’, ‘d’, ‘e’, ‘f’].map(() => “:)”)
-  // -> [‘:)’, ‘:)’, ‘:)’, ‘:)’, ‘:)’ ‘:)’] 인 새 배열을 만들어줌
-  // 다만 기존의 배열에 접근할 수 없게됨
-  // 그러나 map은 함수의 첫 번째 argument로 현재의 item을 가지고 올 수 있음
-  // map(item) -> item이나 원하는 어떤 변수명을 넣으면 item자체를 리턴하는 것도 가능
-  // map((item) => item.toUpperCase())
-  // 로 하면 item이 대문자로 바뀐 새로운 배열은 만들어줌
-  
-  // 리액트는 기본적으로 list에 있는 모든 item을 인식하기 때문에 key를 넣어 고유하게 만들어줘야함
-  // map의 첫 번째 argument는 값이고 두번째는 index 즉 숫자를 의미함
-  // 그래서
-  // {toDos.map((item, index) => {item})}
-  // 만들어줌
-  // 즉,
-  // {{item},{item},{item}...}
-  // 배열을 만들어 각자 고유의 key를 가지게 함
+// 이 경우 coin이라는 변수는 coins 배열 안에 있는 각각의 coin을 의미함
+// 그래서 각각의 coin은 https://api.coinpaprika.com/v1/tickers 처럼 생겼음
+// 그래서 coin.name도 coin.symbol도 가져올 수 있음
 
-import {useState} from "react";
+// 처음에는 기본값으로 비어있는 배열을 넘겨주기 때문에 coin이 처음엔 0개
+// 기본값을 적어도 빈값으로 정해주지 않으면 에러가 남
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]); // 배열의 element를 돌려줌
-    setToDo(""); // 값을 보낼때 사용
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? ( // 로딩 성공시 
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => ( // 로딩 실패시 
+            <option>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
-
 export default App;
---
+++ src/toDo.js
@@ -0,0 +1,62 @@
+
+  // 7-1 투두리스트 만들기 
+
+  // map() 함수
+  // -> 배열을 가지고 있을 때 각각의 element들을 바꿀 수 있게 해줌map() 은 ()에 함수를 넣을 수 있는데 배열의 모든 item에 대해 실행됨
+  // 즉 배열에 6개의 item이 있다면 6번 함수가 실행됨
+  // 그리고 그 함수로부터 내가 return한 값은 새로운 배열에 들어가게 함
+  // [‘a’, ‘b’, ‘c’, ‘d’, ‘e’, ‘f’].map(() => “:)”)
+  // -> [‘:)’, ‘:)’, ‘:)’, ‘:)’, ‘:)’ ‘:)’] 인 새 배열을 만들어줌
+  // 다만 기존의 배열에 접근할 수 없게됨
+  // 그러나 map은 함수의 첫 번째 argument로 현재의 item을 가지고 올 수 있음
+  // map(item) -> item이나 원하는 어떤 변수명을 넣으면 item자체를 리턴하는 것도 가능
+  // map((item) => item.toUpperCase())
+  // 로 하면 item이 대문자로 바뀐 새로운 배열은 만들어줌
+  
+  // 리액트는 기본적으로 list에 있는 모든 item을 인식하기 때문에 key를 넣어 고유하게 만들어줘야함
+  // map의 첫 번째 argument는 값이고 두번째는 index 즉 숫자를 의미함
+  // 그래서
+  // {toDos.map((item, index) => {item})}
+  // 만들어줌
+  // 즉,
+  // {{item},{item},{item}...}
+  // 배열을 만들어 각자 고유의 key를 가지게 함
+
+  import {useState} from "react";
+
+  function App() {
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+      event.preventDefault();
+      if (toDo === "") {
+        return;
+      }
+      setToDos((currentArray) => [toDo, ...currentArray]); // 배열의 element를 돌려줌
+      setToDo(""); // 값을 보낼때 사용
+    };
+    console.log(toDos);
+    return (
+      <div>
+        <h1>My To Dos ({toDos.length})</h1>
+        <form onSubmit={onSubmit}>
+          <input
+            onChange={onChange}
+            value={toDo}
+            type="text"
+            placeholder="Write your to do..."
+          />
+          <button>Add To Do</button>
+        </form>
+        <hr />
+        <ul>
+          {toDos.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  
+  export default App;
