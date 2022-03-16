b12463f7a83d598997a37dcab2778ad947ed515d
jjenny7 <jmlee9707@naver.com>
2022-03-16 19:33:32 +0900

    7-6

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
-
-function Home(){
-    const [loading, setLoading] = useState(true);
+import Movie from "../components/Movie";
+function Home() {
+  const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
     const json = await (
@@ -23,13 +22,18 @@ function Home(){
       ) : (
         <div>
           {movies.map((movie) => (
-          <Movie key = {movie.id}
-          coverImg ={movie.medium_cover_image} title={movie.title} summary ={movie.summary} genres = {movie.genres}/>
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
           ))}
         </div>
       )}
     </div>
   );
 }
-
 export default Home;
